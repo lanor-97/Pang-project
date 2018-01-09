79521a4444f1e367e56824cb0bddcc95432d9dcd
Trieste97 <trsgpp97c29a326r@studenti.unical.it>
2018-01-09 15:21:34 +0100

    solamente aggiustate tabulazioni

--- Palla.h
+++ Palla.h
@@ -16,7 +16,7 @@ private:
 			bouncer,
 			cont,		
 			h_max;		//coeff. altezza massima
-    SIZE size;
+   SIZE size;
   	ALLEGRO_BITMAP *bitmap;
 
 public:
@@ -27,7 +27,7 @@ public:
   	float getY() const  { return posY; }
   	int getDim() const  { return dim; }
   	float getBouncer() const  { return bouncer; }
-    float getCont() const  { return cont; }
+   float getCont() const  { return cont; }
   	void setBitmap(ALLEGRO_BITMAP *b_map)  { bitmap=b_map; }
   	void setX(float x)  { posX=x; }
   	void setY(float y)  { posY=y; }
@@ -41,18 +41,18 @@ Palla::Palla(float c, SIZE s)  {
 	size = s;
 	switch(s)  {
 		case PIC:	dim = 20;
-					h_max = 3;
-					bitmap = al_load_bitmap("images/palla_pic.png");
+						h_max = 3;
+						bitmap = al_load_bitmap("images/palla_pic.png");
 		break;
 		
 		case MED: 	dim = 40;
-					h_max = 2;
-					bitmap = al_load_bitmap("images/palla_med.png");
+						h_max = 2;
+						bitmap = al_load_bitmap("images/palla_med.png");
 		break;
 		
 		case GRA: 	dim = 70;
-					h_max = 1.5;
-					bitmap = al_load_bitmap("images/palla_gra.png");
+						h_max = 1.5;
+						bitmap = al_load_bitmap("images/palla_gra.png");
 		break;
 	}
 	posX = 0;
@@ -62,10 +62,10 @@ Palla::Palla(float c, SIZE s)  {
 }
 
 float Palla::calculateY(const int SY)  { 
-    if(cont > 314)
-		    cont = 1;
+	if(cont > 314)
+		cont = 1;
       
-    float abc = SY-(abs(sin(cont/100))*(SY/h_max))-dim;
+   float abc = SY-(abs(sin(cont/100))*(SY/h_max))-dim;
   	cont += abs(bouncer);
   	return abc;
 }
--- main.cpp
+++ main.cpp
@@ -12,100 +12,96 @@ const int SCREEN_H = 480;
 int main(int argc, char **argv)  {   //int argc e char **argv li devi mettere se no non va a me
 																			//poi a limite li togliamo
 	ALLEGRO_DISPLAY *display = NULL;
-   	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-   	ALLEGRO_TIMER *timer = NULL;
-		ALLEGRO_FONT *font1=NULL;
-		ALLEGRO_FONT *	font2=NULL;
-		ALLEGRO_TRANSFORM redimencionamento;
-		ALLEGRO_MONITOR_INFO info;
+   ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+   ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_FONT *font1=NULL;
+	ALLEGRO_FONT *	font2=NULL;
+	ALLEGRO_TRANSFORM redimencionamento;
+	ALLEGRO_MONITOR_INFO info;
 
-		al_init_font_addon();
-		al_init_ttf_addon();
-   	bool redraw = true;
-   	bool keyRight=false, keyLeft=false, keySpace=false;
+	al_init_font_addon();
+	al_init_ttf_addon();
+   bool redraw = true;
+   bool keyRight=false, keyLeft=false, keySpace=false;
 	bool GameOver=false;
-		int punteggio=0;
-		int tempo=3600;
-		int res_monitor_x;
+	int punteggio=0;
+	int tempo=3600;
+	int res_monitor_x;
 	int res_monitor_y;
 	float res_x;
 	float res_y;
 
-	if(!al_init()) {
+	if(!al_init())  {
     	cerr << "failed to initialize allegro!\n";
     	return -1;
-   	}
+   }
 
-   	if(!al_init_image_addon()) {
-      	cerr<<"failed to initialize allegro_image\n";
+   if(!al_init_image_addon())  {
+     	cerr<<"failed to initialize allegro_image\n";
 		return -1;
-   	}
+   }
 
 
-		if(!al_init_ttf_addon())
-		{
-			cerr<<"failed to initialize allegro_ttf\n";
-			return -1;
-		}
-		if(!al_init_font_addon())
-		{
-			cerr<<"failed to initialize allegro_font\n";
+	if(!al_init_ttf_addon())  {
+		cerr<<"failed to initialize allegro_ttf\n";
+		return -1;
+	}
 
-			return -1;
-		}
+	if(!al_init_font_addon())  {
+		cerr<<"failed to initialize allegro_font\n";
+		return -1;
+	}
 
-		timer = al_create_timer(1.0 / FPS);
-		if(!timer) {
-			cerr<<"failed to create timer!\n";
-			return -1;
-		}
+	timer = al_create_timer(1.0 / FPS);
+	if(!timer) {
+		cerr<<"failed to create timer!\n";
+		return -1;
+	}
 
-		al_get_monitor_info(0,&info);
+	al_get_monitor_info(0,&info);
 	res_monitor_x = info.x2 - info.x1;
 	res_monitor_y = info.y2 - info.y1;
 	res_x = res_monitor_x / (float) SCREEN_W;
 	res_y = res_monitor_y / (float) SCREEN_H;
 	al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
 	display = al_create_display(res_monitor_x,res_monitor_y);
-   	if(!display) {
+   if(!display) {
 		cerr<<"failed to create display!\n";
 		al_destroy_timer(timer);
-      	return -1;
-   	}
+     	return -1;
+   }
 
-		al_identity_transform(&redimencionamento);
+	al_identity_transform(&redimencionamento);
 	al_scale_transform(&redimencionamento,res_x,res_y);
 	al_use_transform(&redimencionamento);
 
-		font1=al_load_ttf_font("images/SHREK.TTF",40,0);
-		if(!font1)
-		{
-			cerr<<"failed to initialize font1\n";
-			al_destroy_timer(timer);
-			return -1;
-		}
+	font1=al_load_ttf_font("images/SHREK.TTF",40,0);
+	if(!font1)  {
+		cerr<<"failed to initialize font1\n";
+		al_destroy_timer(timer);
+		return -1;
+	}
 
-		font2=al_load_ttf_font("images/SHREK.TTF",25,0);
-		if(!font2)
-		{
-			cerr<<"failed to initialize font2\n";
-			al_destroy_timer(timer);
-			return -1;
-		}
+	font2=al_load_ttf_font("images/SHREK.TTF",25,0);
+	if(!font2)  {
+		cerr<<"failed to initialize font2\n";
+		al_destroy_timer(timer);
+		return -1;
+	}
 
 	Palla palla(170, GRA);
-   	if(!palla.getBitmap())  {
+  	if(!palla.getBitmap())  {
 		cerr<<"failed to initialize palla.png!\n";
 		al_destroy_timer(timer);
 		al_destroy_display(display);
 		al_destroy_font(font1);
 		al_destroy_font(font2);
 		return -1;
-   	}
-   	palla.setX(SCREEN_W/2);
-   	palla.setY(palla.calculateY(SCREEN_H));
+   }
+   palla.setX(SCREEN_W/2);
+   palla.setY(palla.calculateY(SCREEN_H));
 
-   	Giocatore player(35,50);
+   Giocatore player(35,50);
 	if(!player.getBitmap())  {
 		cerr<<"failed to initialize man.png!\n";
 		al_destroy_timer(timer);
@@ -113,21 +109,21 @@ int main(int argc, char **argv)  {   //int argc e char **argv li devi mettere se
 		al_destroy_font(font1);
 		al_destroy_font(font2);
 		return -1;
-   	}
-   	player.setX(SCREEN_W/2 - player.getDim_x());
-   	player.setY(SCREEN_H - player.getDim_y());
+   }
+   player.setX(SCREEN_W/2 - player.getDim_x());
+   player.setY(SCREEN_H - player.getDim_y());
 
-   	Arma arma(24,player.getX(), player.getY());
-   	if(!arma.getBitmap())  {
+   Arma arma(24,player.getX(), player.getY());
+   if(!arma.getBitmap())  {
 		cerr<<"failed to initialize arpione.png!\n";
 		al_destroy_timer(timer);
 		al_destroy_display(display);
 		al_destroy_font(font1);
 		al_destroy_font(font2);
 		return -1;
-   	}
+   }
 
-   	Vita vite(3);
+   Vita vite(3);
 	if(!vite.getBitmap())  {
 		cerr<<"failed to initialize cuore.png!\n";
 		al_destroy_timer(timer);
@@ -145,15 +141,15 @@ int main(int argc, char **argv)  {   //int argc e char **argv li devi mettere se
 		al_destroy_font(font2);
 	}
 
-   	event_queue = al_create_event_queue();
-   	if(!event_queue) {
+   event_queue = al_create_event_queue();
+   if(!event_queue) {
 		cerr<<"failed to create event_queue!\n";
 		al_destroy_display(display);
 		al_destroy_timer(timer);
 		al_destroy_font(font1);
 		al_destroy_font(font2);
 		return -1;
-   	}
+   }
 
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_install_keyboard();
@@ -165,9 +161,9 @@ int main(int argc, char **argv)  {   //int argc e char **argv li devi mettere se
 	player.Draw();
 	al_flip_display();
 	al_start_timer(timer);
-   	bool shoot=false, colpito=false, sfondo2=false, presa=false;
+   bool shoot=false, colpito=false, sfondo2=false, presa=false;
 
-   	while(!GameOver) {
+   while(!GameOver) {
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
 		if(shoot==false)  {
@@ -178,53 +174,53 @@ int main(int argc, char **argv)  {   //int argc e char **argv li devi mettere se
 		if(ev.type == ALLEGRO_EVENT_TIMER)  {
 			if(palla.getX() < 0 || palla.getX() > SCREEN_W - palla.getDim()) {
 				palla.setBouncer(-palla.getBouncer());
-			}
+		}
 
-			palla.setX(palla.getX()+palla.getBouncer());
-			palla.setY(palla.calculateY(SCREEN_H));
+		palla.setX(palla.getX()+palla.getBouncer());
+		palla.setY(palla.calculateY(SCREEN_H));
 
-			bool 	ball_hook_1 = arma.getX() <= palla.getX()+palla.getDim(),
-					ball_hook_2 = arma.getX()+arma.getDim() >= palla.getX(),
-					ball_hook_3 = arma.getY() <= palla.getY()+palla.getDim();
+		bool 	ball_hook_1 = arma.getX() <= palla.getX()+palla.getDim(),
+				ball_hook_2 = arma.getX()+arma.getDim() >= palla.getX(),
+				ball_hook_3 = arma.getY() <= palla.getY()+palla.getDim();
 
-			if(ball_hook_1 && ball_hook_2 && ball_hook_3 && !presa)  {
-				//rampino colpisce palla
-				punteggio+=200;
-				presa=true;
-			}
-			if(!ball_hook_1 || !ball_hook_2 || !ball_hook_3)
-				presa=false;
+		if(ball_hook_1 && ball_hook_2 && ball_hook_3 && !presa)  {
+			//rampino colpisce palla
+			punteggio+=200;
+			presa=true;
+		}
+		if(!ball_hook_1 || !ball_hook_2 || !ball_hook_3)
+			presa=false;
 
-			if(keyRight)  {
-				if(player.getX()+player.getDim_x()+5 <= SCREEN_W)
-					player.setX(player.getX()+5);
-				else
-					player.setX(SCREEN_W-player.getDim_x());
-			}
-			if(keyLeft)  {
-				if(player.getX()-5 >= 0)
-					player.setX(player.getX()-5);
-				else
-					player.setX(0);
-			}
-			if(keySpace)  {
-				shoot=true;
-				keySpace=false;
-			}
+		if(keyRight)  {
+			if(player.getX()+player.getDim_x()+5 <= SCREEN_W)
+				player.setX(player.getX()+5);
+			else
+				player.setX(SCREEN_W-player.getDim_x());
+		}
+		if(keyLeft)  {
+			if(player.getX()-5 >= 0)
+				player.setX(player.getX()-5);
+			else
+				player.setX(0);
+		}
+		if(keySpace)  {
+			shoot=true;
+			keySpace=false;
+		}
 
-			bool	ball_player_1 = palla.getX()+palla.getDim() >= player.getX(),
-					ball_player_2 = palla.getX() <= player.getX()+player.getDim_x(),
-					ball_player_3 = player.getY() <= palla.getY()+palla.getDim();
+		bool	ball_player_1 = palla.getX()+palla.getDim() >= player.getX(),
+				ball_player_2 = palla.getX() <= player.getX()+player.getDim_x(),
+				ball_player_3 = player.getY() <= palla.getY()+palla.getDim();
 
-			if(ball_player_1 && ball_player_2 && ball_player_3 && !colpito)  {
-				//palla colpisce player
-				colpito=true;
-				vite--;
-			}
-			if(!ball_player_1 || !ball_player_2 || !ball_player_3)
-				colpito=false;
+		if(ball_player_1 && ball_player_2 && ball_player_3 && !colpito)  {
+			//palla colpisce player
+			colpito=true;
+			vite--;
+		}
+		if(!ball_player_1 || !ball_player_2 || !ball_player_3)
+			colpito=false;
 
-			redraw = true;
+		redraw = true;
 		}
 		else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			break;
@@ -246,7 +242,6 @@ int main(int argc, char **argv)  {   //int argc e char **argv li devi mettere se
 		}
 
 		if(redraw && al_is_event_queue_empty(event_queue)) {
-
 			if(vite.getNumVite() == 2 && !sfondo2)  {
 				sfondo.setBitmap(al_load_bitmap("images/sfondo2.png"));
 				if(!sfondo.getBitmap())  {
@@ -285,9 +280,9 @@ int main(int argc, char **argv)  {   //int argc e char **argv li devi mettere se
 	}
 	al_destroy_font(font1);
 	al_destroy_font(font2);
-   	al_destroy_timer(timer);
-   	al_destroy_display(display);
-   	al_destroy_event_queue(event_queue);
+   al_destroy_timer(timer);
+  	al_destroy_display(display);
+  	al_destroy_event_queue(event_queue);
 
-   	return 0;
+  	return 0;
 }
